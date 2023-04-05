cf12cea1865115e8b0c789d522e14ed752904837
yao <lu.yao@bestskip.com>
2023-04-05 10:15:57 +0900

    CHG config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    open: './src/frontend/index.html',
+    open: 'src/index.html',
+    port: 3000,
   },
 })

79aef6c12f8b2cf83c7e5ac2de6de292acab719e
yao <lu.yao@bestskip.com>
2023-04-11 15:44:58 +0900

    CHG code lint

--- src/editor.vue
+++ src/editor.vue
@@ -1,41 +1,41 @@
 <script setup lang="ts">
-import {EditorState} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
-import {Schema, DOMParser, Node} from "prosemirror-model"
-import {keymap} from "prosemirror-keymap"
-import {baseKeymap} from "./commands"
-import { ref, onMounted } from 'vue'
-import {schema} from "./schema"
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { Schema, DOMParser, Node } from 'prosemirror-model';
+import { keymap } from 'prosemirror-keymap';
+import { baseKeymap } from './commands';
+import { ref, onMounted } from 'vue';
+import { schema } from './schema';
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
 const mySchema = new Schema({
   nodes: schema.spec.nodes,
-  marks: schema.spec.marks
-})
+  marks: schema.spec.marks,
+});
 
 // lifecycle hooks
 onMounted(() => {
-  const view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({
-    doc: Node.fromJSON(mySchema, {
-      type: "doc",
-      content: [
-        {
-          type: "paragraph",
-          content: [
-            {
-              type: "text",
-              text: "Hello world!"
-            }
-          ]
-        }
-      ]
+  const view = new EditorView(document.querySelector('#editor'), {
+    state: EditorState.create({
+      doc: Node.fromJSON(mySchema, {
+        type: 'doc',
+        content: [
+          {
+            type: 'paragraph',
+            content: [
+              {
+                type: 'text',
+                text: 'Hello world!',
+              },
+            ],
+          },
+        ],
+      }),
+      plugins: [keymap(baseKeymap)],
     }),
-    plugins: [keymap(baseKeymap)]
-  })
-})
-})
+  });
+});
 </script>
 
 <template>

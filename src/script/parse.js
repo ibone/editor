f7f216b770ce9af0fc2e34bbc6c2baa509e16c43
ibone <water2212683@gmail.com>
2023-04-27 13:43:40 +0900

    feat(parse): update

--- src/script/parse.js
+++ src/script/parse.js
@@ -18,10 +18,90 @@ function main() {
     readFile(path.join(__dirname, '../commands/.commands'))
         .then(function (data) {
         var arr = data.split('\n');
-        console.log(arr);
+        // console.log(arr);
+        var lines = [];
+        arr.forEach(function (item) {
+            var matchResult = item.match(/^\s*/) || [''];
+            var indent = matchResult[0].length / 4;
+            var command = item.replace(/^\s*/, '');
+            var symbol = command.match(/(&&|=>)/);
+            if (command) {
+                lines.push({
+                    indent: indent,
+                    command: item.replace(/(&&|=>)/, '').replace(/^\s*|\s*$/, ''),
+                    closed: false,
+                    symbol: symbol ? symbol[0] : '',
+                });
+            }
+        });
+        var commands = getCommands(lines, 0, [], [], 0);
+        console.log(commands);
     })
         .catch(function (err) {
         console.log(err);
     });
 }
+function getCommands(lines, indent, command, commands, index) {
+    var line = lines[index];
+    if (!line) {
+        return commands;
+    }
+    if (line.closed) {
+        return getCommands(lines, indent, command, commands, index + 1);
+    }
+    else if (line.indent === indent) {
+        if (line.symbol === '&&') {
+            addCommand(command, line, indent);
+            return getCommands(lines, indent, command, commands, index + 1);
+        }
+        else if (line.symbol === '=>') {
+            addCommand(command, line, indent);
+            setClose(command, indent - 1);
+            commands.push(command);
+            return getCommands(lines, 0, [], commands, 0);
+        }
+        else {
+            addCommand(command, line, indent);
+            return getCommands(lines, indent + 1, command, commands, index + 1);
+        }
+    }
+    else if (line.indent < indent) {
+        setClose(command, indent - 1);
+        return getCommands(lines, 0, [], commands, 0);
+    }
+    else {
+        line.closed = true;
+        return getCommands(lines, indent, command, commands, index + 1);
+    }
+}
+function addCommand(command, line, indent) {
+    var commandInfo = command[indent];
+    if (!commandInfo) {
+        command[indent] = {
+            indent: indent,
+            lines: [line],
+        };
+    }
+    else {
+        commandInfo.lines.push(line);
+    }
+}
+function setClose(command, level) {
+    var commandInfo = command[level];
+    commandInfo.lines.forEach(function (line) {
+        line.closed = true;
+    });
+}
+/**
+ * 1. 当前行indent与指定indent相等
+ *     1.1 遇到&&，将index加1，往下查找
+ *     1.2 遇到=>，命令查找结束，将command加入commands，将index加1，往下查找
+ *     1.3 默认操作，将index和indent都加1，往下查找
+ * 2. 当前行indent大于指定indent
+ *     2.1 查找到指定indent的底了，需要将指定indent的parent line做关闭
+ * 3. 当前行indent小于指定indent
+ *     3.1 格式错误，对当前行做关闭
+ * 4. 当前行如果关闭
+ *     4.1 将index加1，往下查找
+ */
 main();
--- src/script/parse.ts
+++ src/script/parse.ts
@@ -13,9 +13,8 @@ function readFile(path: string) {
   });
 }
 
-const symbols = ['&&', '=>'];
 interface Line {
-  level: number;
+  indent: number;
   command: string;
   symbol: string;
   closed: boolean;
@@ -28,67 +27,96 @@ function main() {
       const lines: Line[] = [];
       arr.forEach((item) => {
         const matchResult = item.match(/^\s*/) || [''];
-        const level = matchResult[0].length / 4;
+        const indent = matchResult[0].length / 4;
         const command = item.replace(/^\s*/, '');
-        const symbol = command.match(/^(&& | =>)/);
+        const symbol = command.match(/(&&|=>)/);
         if (command) {
           lines.push({
-            level: level,
-            command: item.replace(/^\s*/, ''),
-            closed: true,
+            indent: indent,
+            command: item.replace(/(&&|=>)/, '').replace(/^\s*|\s*$/, ''),
+            closed: false,
             symbol: symbol ? symbol[0] : '',
           });
         }
       });
-      const commands = getCommand(lines, 0, [], [], 0);
+      const commands = getCommands(lines, 0, [], [], 0);
+      console.log(commands);
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-function getCommand(
+interface LineInfo {
+  indent: number;
+  lines: Line[];
+}
+
+function getCommands(
   lines: Line[],
   indent: number,
-  command: string[],
-  commands: string[][],
-  index: number,
-  parentIndex?: number
-) {
+  command: LineInfo[],
+  commands: LineInfo[][],
+  index: number
+): LineInfo[][] {
   const line = lines[index];
-  if (line.level === indent) {
-    if (line.closed) {
-      getCommand(lines, indent, command, commands, index + 1);
-    } else if (line.symbol === '&&') {
-      getCommand(lines, indent, command, commands, index + 1);
-    } else if (line.symbol === '=>') {
-      commands.push(command);
-      getCommand(lines, 0, [], commands, 0);
-    } else {
-      command.push(line.command);
-      getCommand(lines, indent + 1, command, commands, index + 1);
-    }
-  } else {
-    if (line.level > indent) {
-      if (typeof parentIndex === 'number') {
-        lines[parentIndex].closed = true;
-      }
-    } else {
-      line.closed = true;
-      getCommand(lines, 0, [], commands, 0);
-    }
+  if (!line) {
+    return commands;
   }
+  if (line.closed) {
+    return getCommands(lines, indent, command, commands, index + 1);
+  } else if (line.indent === indent) {
+    if (line.symbol === '&&') {
+      addCommand(command, line, indent);
+      return getCommands(lines, indent, command, commands, index + 1);
+    } else if (line.symbol === '=>') {
+      addCommand(command, line, indent);
+      setClose(command, indent - 1);
+      commands.push(command);
+      return getCommands(lines, 0, [], commands, 0);
+    } else {
+      addCommand(command, line, indent);
+      return getCommands(lines, indent + 1, command, commands, index + 1);
+    }
+  } else if (line.indent < indent) {
+    setClose(command, indent - 1);
+    return getCommands(lines, 0, [], commands, 0);
+  } else {
+    line.closed = true;
+    return getCommands(lines, indent, command, commands, index + 1);
+  }
+}
+
+function addCommand(command: LineInfo[], line: Line, indent: number) {
+  const commandInfo = command[indent];
+  if (!commandInfo) {
+    command[indent] = {
+      indent: indent,
+      lines: [line],
+    };
+  } else {
+    commandInfo.lines.push(line);
+  }
+}
+
+function setClose(command: LineInfo[], level: number) {
+  const commandInfo = command[level];
+  commandInfo.lines.forEach((line) => {
+    line.closed = true;
+  });
 }
 
 /**
  * 1. 当前行indent与指定indent相等
  *     1.1 遇到&&，将index加1，往下查找
- *     1.2 遇到=>，命令查找结束，将command加入commands，并从头开始查找
+ *     1.2 遇到=>，命令查找结束，将command加入commands，将index加1，往下查找
  *     1.3 默认操作，将index和indent都加1，往下查找
  * 2. 当前行indent大于指定indent
  *     2.1 查找到指定indent的底了，需要将指定indent的parent line做关闭
  * 3. 当前行indent小于指定indent
  *     3.1 格式错误，对当前行做关闭
+ * 4. 当前行如果关闭
+ *     4.1 将index加1，往下查找
  */
 
 main();
